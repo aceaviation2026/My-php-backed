81841bb676d3e7756cb55d13347df0b8658ca274
aceaviation2026 <aceaviation2026@gmail.com>
2026-07-01 11:46:59 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,16 +1,31 @@
+// <?php
+
+// $host = "127.0.0.1";
+// $username = "root";
+// $password = "";
+// $database = "aviation_db";
+
+// $conn = new mysqli(
+//     $host,
+//     $username,
+//     $password,
+//     $database
+// );
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// ?>
+
 <?php
 
-$host = "127.0.0.1";
-$username = "root";
-$password = "";
-$database = "aviation_db";
+$host = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
 
-$conn = new mysqli(
-    $host,
-    $username,
-    $password,
-    $database
-);
+$conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
